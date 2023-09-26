239845b0c0e18e141200a5541041c62da3140823
Jone <jone.chong99@gmail.com>
2023-09-26 22:47:27 +0800

    feat: Added button svgs to assets folder

--- components/FoodDisplay/FoodDisplay.js
+++ components/FoodDisplay/FoodDisplay.js
@@ -1,13 +1,20 @@
 import { StyleSheet, View } from "react-native";
+import { useState } from "react";
 import DateSelector from "../DateSelector";
 import FoodList from "./FoodView/FoodList/FoodList";
 import FoodView from "./FoodView/FoodView";
 
 export default function FoodDisplay() {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const handleDateChange = (newDate) => {
+        setSelectedDate(newDate);
+    };
+
     return (
         <View style={styles.container}>
-            <DateSelector />
-            <FoodView />
+            <DateSelector onDateChange={handleDateChange} />
+            <FoodView date={selectedDate} />
         </View>
     );
 }
