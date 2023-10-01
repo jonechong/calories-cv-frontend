cbc9c950db092b713722765779c5d7d9d182678a
Jone <jone.chong99@gmail.com>
2023-10-02 00:12:53 +0800

    feat: added props from parents for date selection

--- components/DateSelector.js
+++ components/DateSelector.js
@@ -2,35 +2,35 @@ import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { IconButton, Subheading } from "react-native-paper";
 
-export default function DateSelector() {
-    const currentDate = new Date();
+export default function DateSelector({ date, onDateChange }) {
+    const [currentDate, setCurrentDate] = useState(date);
     const currentMonth = currentDate.getMonth() + 1;
     const currentDay = currentDate.getDate();
     const currentYear = currentDate.getFullYear();
 
-    const [selectedDate, setSelectedDate] = useState(new Date());
-
     const decreaseDate = () => {
-        const dateCopy = new Date(selectedDate);
-        dateCopy.setDate(dateCopy.getDate() - 1);
-        setSelectedDate(dateCopy);
+        const newDate = new Date(currentDate);
+        newDate.setDate(newDate.getDate() - 1);
+        setCurrentDate(newDate);
+        onDateChange(newDate);
     };
 
     const increaseDate = () => {
-        const dateCopy = new Date(selectedDate);
-        dateCopy.setDate(dateCopy.getDate() + 1);
-        setSelectedDate(dateCopy);
+        const newDate = new Date(currentDate);
+        newDate.setDate(newDate.getDate() + 1);
+        setCurrentDate(newDate);
+        onDateChange(newDate);
     };
 
     return (
         <View style={styles.container}>
-            <IconButton icon="chevron-left" onPress={increaseDate}></IconButton>
+            <IconButton icon="chevron-left" onPress={decreaseDate}></IconButton>
             <Subheading style={{ color: "black" }}>
                 {currentDay}/{currentMonth}/{currentYear}
             </Subheading>
             <IconButton
                 icon="chevron-right"
-                onPress={decreaseDate}
+                onPress={increaseDate}
             ></IconButton>
         </View>
     );
@@ -43,7 +43,6 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         alignItems: "center", // vertically center items
         justifyContent: "center", // horizontally center items
-        // justifyContent: "space-between", // space items out evenly
         paddingHorizontal: 10, // for a little padding on the sides
     },
 });
