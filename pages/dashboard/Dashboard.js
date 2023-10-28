37c6259f5921db376bfec9759038b0c61c008ab4
Jone <jone.chong99@gmail.com>
2023-10-28 13:48:19 +0800

    feat: changed View components to Surface components

--- pages/dashboard/Dashboard.js
+++ pages/dashboard/Dashboard.js
@@ -2,30 +2,23 @@ import { StyleSheet, View, useWindowDimensions } from "react-native";
 import DashboardButtons from "../../components/Dashboard/DashboardButtons";
 import DashboardHeader from "../../components/Dashboard/DashboardHeader";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
+import { useTheme, Surface } from "react-native-paper";
 
 export default function Dashboard() {
+    const theme = useTheme();
+
     //Add margin if screen size is large
     const { width } = useWindowDimensions();
     const breakpoint = 768;
     const marginSize = width > breakpoint ? width * 0.2 : 0; //0.2 corresponds to 20%
 
     return (
-        <View style={styles.container}>
+        <Surface>
             <DashboardHeader />
-            <View style={{ marginHorizontal: marginSize }}>
+            <Surface style={{ marginHorizontal: marginSize }}>
                 <FoodDisplay />
                 <DashboardButtons />
-            </View>
-        </View>
+            </Surface>
+        </Surface>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: "column",
-        alignContent: "center",
-        backgroundColor: "#fff",
-    },
-    spacer: { width: "20%" },
-});
