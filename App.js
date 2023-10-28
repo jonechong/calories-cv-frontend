f2d0f434d0b344259f614cb05d6c99b018d5e5a1
Jone <jone.chong99@gmail.com>
2023-10-28 16:12:07 +0800

    feat: added Light and Dark themes

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Dashboard from "./pages/dashboard/Dashboard";
-import { DarkTheme, PaperProvider } from "react-native-paper";
+import { PaperProvider } from "react-native-paper";
 import { useEffect } from "react";
-import theme from "./theme"; // Import the theme configuration
+import LightTheme from "./LightTheme";
+import DarkTheme from "./DarkTheme";
 
 export default function App() {
     return (
-        <PaperProvider theme={theme}>
+        <PaperProvider theme={DarkTheme}>
             <Dashboard />
         </PaperProvider>
     );
---
+++ DarkTheme.js
@@ -0,0 +1,43 @@
+// theme.js
+import { DefaultTheme } from "react-native-paper";
+
+const DarkTheme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: "#D0BCFF",
+        onPrimary: "#381E72",
+        primaryContainer: "#4F378B",
+        onPrimaryContainer: "#EADDFF",
+        secondary: "#CCC2DC",
+        onSecondary: "#332D41",
+        secondaryContainer: "#4A4458",
+        onSecondaryContainer: "#E8DEF8",
+        tertiary: "#EFB8C8",
+        onTertiary: "#492532",
+        tertiaryContainer: "#633B48",
+        onTertiaryContainer: "#FFD8E4",
+        error: "#F2B8B5",
+        onError: "#601410",
+        errorContainer: "#8C1D18",
+        onErrorContainer: "#F9DEDC",
+        background: "#1C1B1F",
+        onBackground: "#E6E1E5",
+        surface: "#1C1B1F",
+        onSurface: "#E6E1E5",
+        outline: "#938F99",
+        surfaceVariant: "#49454F",
+        onSurfaceVariant: "#CAC4D0",
+        elevation: {
+            level0: "transparent",
+            level1: "rgb(247, 243, 249)",
+            level2: "rgb(243, 237, 246)",
+            level3: "rgb(238, 232, 244)",
+            level4: "rgb(236, 230, 243)",
+            level5: "rgb(233, 227, 241)",
+        },
+    },
+    dark: "true",
+};
+
+export default DarkTheme;
