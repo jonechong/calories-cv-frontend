38da71f0512d067277ef9c9c621ddf6c30c2676c
Jone <jone.chong99@gmail.com>
2023-10-29 22:56:22 +0800

    feat: moved styles into the local component function

--- components/FoodDisplay/FoodView/FoodView.js
+++ components/FoodDisplay/FoodView/FoodView.js
@@ -1,20 +1,26 @@
 import { StyleSheet, View } from "react-native";
 import FoodList from "./FoodList/FoodList";
+import { Card, Surface, useTheme } from "react-native-paper";
 
-export default function FoodView() {
+export default function FoodView({ date }) {
+    const theme = useTheme();
+
+    const styles = StyleSheet.create({
+        container: {
+            flexDirection: "column",
+            alignContent: "center",
+            alignItems: "center",
+            padding: "2.5%",
+            margin: "4%",
+            backgroundColor: theme.colors.secondaryContainer,
+        },
+    });
+
     return (
-        <View style={styles.container}>
+        <Card style={styles.container}>
             <FoodList />
             <FoodList />
             <FoodList />
-        </View>
+        </Card>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-        flexDirection: "column",
-        backgroundColor: "#fff",
-    },
-});
