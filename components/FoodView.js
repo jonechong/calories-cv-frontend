52e003ffac747e841835e742e1d79c50608432c4
Jone <jone.chong99@gmail.com>
2023-11-04 18:09:47 +0800

    fix: implemented re-render on return navigation

--- components/FoodView.js
+++ components/FoodView.js
@@ -1,128 +1,137 @@
-import React, { useState } from "react";
-import { useMemo } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { StyleSheet, View, Image } from "react-native";
 import { List, useTheme, Card, TouchableRipple } from "react-native-paper";
 import * as SQLite from "expo-sqlite";
-import { useEffect } from "react";
+import { useFocusEffect } from "@react-navigation/core";
 
 export default function FoodView({ date }) {
     const theme = useTheme();
-    const styles = useMemo(() => {
-        return StyleSheet.create({
-            foodList: { width: "100%" },
-            foodView: {
-                // flexDirection: "column",
-                padding: "1%",
-                margin: "4%",
-                backgroundColor: theme.colors.secondaryContainer,
-            },
-            touchable: {
-                width: "100%",
-                paddingHorizontal: "4%",
-                borderRadius: 10,
-                overflow: "hidden",
-            },
-        });
-    }, [theme]);
+    const styles = StyleSheet.create({
+        foodList: { width: "100%" },
+        foodView: {
+            padding: "1%",
+            margin: "4%",
+            backgroundColor: theme.colors.secondaryContainer,
+        },
+        touchable: {
+            width: "100%",
+            paddingHorizontal: "4%",
+            borderRadius: 10,
+            overflow: "hidden",
+        },
+        image: {
+            height: 50, // set a fixed height for images
+            aspectRatio: 1,
+            borderRadius: 10,
+        },
+    });
+
+    const [foodData, setFoodData] = useState([]);
+    const [imageLoadError, setImageLoadError] = useState({});
     const [listItemHeight, setListItemHeight] = React.useState(null);
     const onListItemLayout = (event) => {
         const { height } = event.nativeEvent.layout;
         setListItemHeight(height);
     };
+    const altImg = require("../assets/no_img_avail.png");
+
+    const fetchDb = useCallback(async (queryDate) => {
+        const db = SQLite.openDatabase("calories-cv.db");
+        const dateString = queryDate.toISOString().split("T")[0];
+        try {
+            const result = await new Promise((resolve, reject) => {
+                db.transaction((tx) => {
+                    tx.executeSql(
+                        "SELECT * FROM logged_food WHERE food_date = date(?);",
+                        [dateString],
+                        (_, { rows }) => resolve(rows._array),
+                        (_, error) => {
+                            reject(error);
+                            return false;
+                        }
+                    );
+                });
+            });
+            return result;
+        } catch (error) {
+            console.error("Failed to fetch records:", error);
+            throw error;
+        }
+    }, []);
 
     const editItem = (item) => {
         console.log("Edit item:", item);
     };
 
-    const fetchDb = async (date) => {
-        return new Promise((resolve, reject) => {
-            const db = SQLite.openDatabase("calories-cv.db");
-            const dateString = date.toISOString().split("T")[0];
-            db.transaction((tx) => {
-                tx.executeSql(
-                    "SELECT * FROM logged_food WHERE food_date = date(?);",
-                    [dateString],
-                    (_, { rows }) => {
-                        resolve(rows._array);
-                    },
-                    (_, error) => {
-                        reject(error);
-                        return false;
+    useFocusEffect(
+        useCallback(() => {
+            let isActive = true;
+
+            fetchDb(date)
+                .then((records) => {
+                    if (isActive) {
+                        console.log(records);
+                        setFoodData(records);
                     }
-                );
-            });
-        });
-    };
+                })
+                .catch(console.error);
 
-    const [foodData, setFoodData] = useState([]);
-    const [altImg, setAltImg] = useState();
-    const [imageLoadError, setImageLoadError] = useState(false);
+            return () => {
+                isActive = false;
+            };
+        }, [date, fetchDb])
+    );
 
-    useEffect(() => {
-        setAltImg(require("../assets/no_img_avail.png"));
-        fetchDb(date)
-            .then((records) => {
-                setFoodData(records);
-                console.log("Fetched records:", records);
-            })
-            .catch((error) => {
-                console.error("Failed to fetch records:", error);
-            });
+    const handleImageError = useCallback((index) => {
+        setImageLoadError((prevError) => ({
+            ...prevError,
+            [index]: true,
+        }));
     }, []);
 
-    const renderFoodItem = (item, index) => {
-        const { food_name, calories, protein, carbs, fats, image_uri } = {
-            ...item,
-            protein: item.protein || "N/A",
-            carbs: item.carbs || "N/A",
-            fats: item.fats || "N/A",
-        };
+    const renderFoodItem = useCallback(
+        (item, index) => {
+            const { food_name, calories, protein, carbs, fats, image_uri } =
+                item;
+            const imageSource =
+                imageLoadError[index] || !image_uri
+                    ? altImg
+                    : { uri: image_uri };
 
-        const handleImageError = (index) => {
-            setImageLoadError((prevError) => ({
-                ...prevError,
-                [index]: true,
-            }));
-        };
-
-        const imageSource =
-            imageLoadError[index] || !item.image_uri
-                ? altImg
-                : { uri: item.image_uri };
-
-        return (
-            <TouchableRipple
-                key={index}
-                onPress={() => editItem(item)}
-                style={styles.touchable}
-                rippleColor={theme.colors.onSurface + "1F"} // Ripple color with low opacity
-                borderless={true}
-            >
-                <List.Item
-                    key={index}
-                    title={`${food_name}`}
-                    description={`Calories: ${calories} kcal\nProtein: ${protein} g\nCarbs: ${carbs} g\nFats: ${fats} g`}
-                    descriptionNumberOfLines={4}
-                    left={() => (
-                        <View onLayout={onListItemLayout}>
-                            {listItemHeight && (
-                                <Image
-                                    source={imageSource}
-                                    onError={() => handleImageError(index)}
-                                    style={{
-                                        height: listItemHeight,
-                                        aspectRatio: 1,
-                                        borderRadius: 10,
-                                    }}
-                                    resizeMode="contain"
-                                />
-                            )}
-                        </View>
-                    )}
-                />
-            </TouchableRipple>
-        );
-    };
+            return (
+                <TouchableRipple
+                    key={index.toString()}
+                    onPress={() => editItem(item)}
+                    style={styles.touchable}
+                    rippleColor={`${theme.colors.onSurface}1F`} // Ripple color with low opacity
+                    borderless={true}
+                >
+                    <List.Item
+                        title={food_name}
+                        description={`Calories: ${calories} kcal\nProtein: ${protein} g\nCarbs: ${carbs} g\nFats: ${fats} g`}
+                        descriptionNumberOfLines={4}
+                        left={() => (
+                            <View onLayout={onListItemLayout}>
+                                {listItemHeight && (
+                                    <Image
+                                        source={imageSource}
+                                        onError={() => handleImageError(index)}
+                                        style={{
+                                            height: listItemHeight,
+                                            aspectRatio: 1,
+                                            borderRadius: 10,
+                                        }}
+                                        resizeMode="contain"
+                                    />
+                                )}
+                            </View>
+                        )}
+                    />
+                </TouchableRipple>
+            );
+        },
+        [imageLoadError, altImg, editItem, theme.colors]
+    );
 
     return (
         <Card style={styles.foodView}>
