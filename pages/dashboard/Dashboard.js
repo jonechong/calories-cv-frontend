d17198c15d879695b76c1be998742050a6e8ecff
Jone <jone.chong99@gmail.com>
2023-11-04 18:10:54 +0800

    chore/test: added comments for testing purposes

--- pages/dashboard/Dashboard.js
+++ pages/dashboard/Dashboard.js
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { StyleSheet, View, useWindowDimensions } from "react-native";
 import DashboardButtons from "../../components/Dashboard/DashboardButtons";
 import DashboardHeader from "../../components/Dashboard/DashboardHeader";
-import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
-import { useTheme, Surface, Button } from "react-native-paper";
-import FoodView from "../../components/FoodDisplay/FoodView/FoodView";
+import { useTheme, Card, Button } from "react-native-paper";
 import { useMemo } from "react";
+import FoodView from "../../components/FoodView";
+import * as SQLite from "expo-sqlite";
 
-export default function Dashboard({ navigation }) {
+export default function Dashboard() {
     const theme = useTheme();
     const styles = useMemo(() => {
         return StyleSheet.create({
@@ -20,6 +20,12 @@ export default function Dashboard({ navigation }) {
                 marginHorizontal: marginSize,
                 backgroundColor: theme.colors.background,
             },
+            foodView: {
+                flexDirection: "column",
+                paddingHorizontal: "4%",
+                margin: "4%",
+                backgroundColor: theme.colors.secondaryContainer,
+            },
         });
     }, [theme]);
 
@@ -27,12 +33,172 @@ export default function Dashboard({ navigation }) {
     const { width } = useWindowDimensions();
     const breakpoint = 768;
     const marginSize = width > breakpoint ? width * 0.2 : 0; //0.2 corresponds to 20%
-
     const [selectedDate, setSelectedDate] = useState(new Date());
 
     const handleDateChange = (newDate) => {
         setSelectedDate(newDate);
     };
+
+    // const createDb = () => {
+    //     // Open a database connection. If the database does not exist, it will be created.
+    //     const db = SQLite.openDatabase("calories-cv.db");
+
+    //     db.transaction((tx) => {
+    //         // Execute the SQL statement to create a table
+    //         tx.executeSql(
+    //             `CREATE TABLE IF NOT EXISTS logged_food (
+    //           log_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    //           food_name TEXT NOT NULL,
+    //           food_date DATE NOT NULL,
+    //           calories INTEGER NOT NULL,
+    //           protein INTEGER,
+    //           fats INTEGER,
+    //           carbs INTEGER,
+    //           image_uri TEXT
+    //         );`,
+    //             [],
+    //             () => {
+    //                 console.log("Table created successfully");
+    //             },
+    //             (_, error) => {
+    //                 console.log("Failed to create table", error);
+    //             }
+    //         );
+    //     });
+    // };
+
+    // const clearDb = () => {
+    //     const db = SQLite.openDatabase("calories-cv.db");
+
+    //     db.transaction((tx) => {
+    //         // This will delete all entries in the logged_food table
+    //         tx.executeSql(
+    //             "DELETE FROM logged_food;",
+    //             [],
+    //             (_, result) => {
+    //                 console.log("All records deleted", result);
+    //             },
+    //             (_, error) => {
+    //                 console.log("Failed to delete records", error);
+    //             }
+    //         );
+    //     });
+    // };
+
+    // const deleteDb = () => {
+    //     const db = SQLite.openDatabase("calories-cv.db");
+
+    //     db.transaction((tx) => {
+    //         // This will delete all entries in the logged_food table
+    //         tx.executeSql(
+    //             "DROP TABLE logged_food;",
+    //             [],
+    //             (_, result) => {
+    //                 console.log("Table deleted", result);
+    //             },
+    //             (_, error) => {
+    //                 console.log("Failed to delete table", error);
+    //             }
+    //         );
+    //     });
+    // };
+
+    // const queryDb = () => {
+    //     const fetchAllRecords = async () => {
+    //         return new Promise((resolve, reject) => {
+    //             const db = SQLite.openDatabase("calories-cv.db");
+
+    //             db.transaction((tx) => {
+    //                 tx.executeSql(
+    //                     "SELECT * FROM logged_food;",
+    //                     [],
+    //                     (_, { rows }) => {
+    //                         resolve(rows._array); // Assuming you want to resolve the array of records
+    //                     },
+    //                     (_, error) => {
+    //                         reject(error); // In case of an error, reject the promise
+    //                         return false; // To stop the propagation of the error
+    //                     }
+    //                 );
+    //             });
+    //         });
+    //     };
+
+    //     // Usage:
+    //     fetchAllRecords()
+    //         .then((records) => {
+    //             console.log("Fetched records:", records);
+    //         })
+    //         .catch((error) => {
+    //             console.error("Failed to fetch records:", error);
+    //         });
+    // };
+
+    // const insertDb = () => {
+    //     const insertDb = async (
+    //         foodName,
+    //         foodDate,
+    //         calories,
+    //         protein,
+    //         fats,
+    //         carbs,
+    //         imageUri
+    //     ) => {
+    //         return new Promise((resolve, reject) => {
+    //             const db = SQLite.openDatabase("calories-cv.db");
+
+    //             db.transaction((tx) => {
+    //                 tx.executeSql(
+    //                     `INSERT INTO logged_food (food_name, food_date, calories, protein, fats, carbs, image_uri)
+    //               VALUES (?, ?, ?, ?, ?, ?, ?);`,
+    //                     [
+    //                         foodName,
+    //                         foodDate,
+    //                         calories,
+    //                         protein,
+    //                         fats,
+    //                         carbs,
+    //                         imageUri,
+    //                     ],
+    //                     (_, result) => {
+    //                         resolve(result); // If insert is successful, resolve the promise
+    //                     },
+    //                     (_, error) => {
+    //                         reject(error); // If there's an error, reject the promise
+    //                         return false; // To stop the propagation of the error
+    //                     }
+    //                 );
+    //             });
+    //         });
+    //     };
+
+    //     const foodData = {
+    //         foodName: "Apple",
+    //         foodDate: "2023-11-04",
+    //         calories: 95,
+    //         protein: 0,
+    //         fats: 0,
+    //         carbs: 25,
+    //         imageUri:
+    //             "https://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/asteroid_blend.png",
+    //     };
+
+    //     insertDb(
+    //         foodData.foodName,
+    //         foodData.foodDate,
+    //         foodData.calories,
+    //         foodData.protein,
+    //         foodData.fats,
+    //         foodData.carbs,
+    //         foodData.imageUri
+    //     )
+    //         .then((result) => {
+    //             console.log("Record inserted successfully", result);
+    //         })
+    //         .catch((error) => {
+    //             console.error("Failed to insert record:", error);
+    //         });
+    // };
 
     return (
         <View style={styles.container}>
@@ -44,6 +210,21 @@ export default function Dashboard({ navigation }) {
                 <FoodView date={selectedDate} />
             </View>
             <DashboardButtons />
+            {/* <Button mode="contained" onPress={createDb}>
+                Database cr8
+            </Button>
+            <Button mode="contained" onPress={clearDb}>
+                Database clear
+            </Button>
+            <Button mode="contained" onPress={deleteDb}>
+                Database delete
+            </Button>
+            <Button mode="contained" onPress={queryDb}>
+                Database query
+            </Button>
+            <Button mode="contained" onPress={insertDb}>
+                Database insert
+            </Button> */}
         </View>
     );
 }
