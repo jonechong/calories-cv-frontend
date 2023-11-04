d8ef0ee9ad65d2b3757d1148c6d5ce42f0947099
Jone <jone.chong99@gmail.com>
2023-11-04 13:09:03 +0800

    feat: added data validation check and changed cancel button colour

--- components/AddFood/AddFoodButtons.js
+++ components/AddFood/AddFoodButtons.js
@@ -5,29 +5,74 @@ import {
     Image,
     Dimensions,
 } from "react-native";
-import { Text, Button, useTheme } from "react-native-paper";
+import {
+    Text,
+    Button,
+    Dialog,
+    Paragraph,
+    Portal,
+    useTheme,
+} from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
+import { useMemo, useState } from "react";
 
-export default function AddFoodButtons() {
+export default function AddFoodButtons({ foodName, foodDate, macroData }) {
     const navigation = useNavigation();
     const theme = useTheme();
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            width: "100%",
-            padding: "2.5%",
-            marginBottom: "3%",
-            flexDirection: "row",
-            alignContent: "center",
-            alignItems: "flex-end",
-            justifyContent: "space-evenly",
-        },
-        button: {
-            width: "33%",
-        },
-    });
+    const styles = useMemo(() => {
+        return StyleSheet.create({
+            container: {
+                flex: 1,
+                width: "100%",
+                padding: "2.5%",
+                marginBottom: "3%",
+                flexDirection: "row",
+                alignContent: "center",
+                alignItems: "flex-end",
+                justifyContent: "space-evenly",
+            },
+            button: {
+                width: "33%",
+            },
+        });
+    }, [theme]);
+
+    const [isDialogVisible, setDialogVisible] = useState(false);
+
+    const validateInput = () => {
+        if (!foodName.trim()) {
+            Alert.alert("Validation Error", "Please enter a food name.");
+            return false;
+        }
+
+        if (!foodDate) {
+            Alert.alert("Validation Error", "Please select a date.");
+            return false;
+        }
+
+        if (!macroData.calories.trim()) {
+            Alert.alert(
+                "Validation Error",
+                "Please enter the number of calories."
+            );
+            return false;
+        }
+
+        return true;
+    };
+
     const submit = () => {
+        const foodData = { foodName, foodDate, ...macroData };
+        console.log(foodData);
         console.log("Submit Pressed");
+
+        // Trim the values and check if they are not empty
+        if (!foodName.trim() || !foodDate || !macroData.calories.trim()) {
+            // Set the state to true to show the dialog
+            setDialogVisible(true);
+        } else {
+            // Proceed with your submit logic
+        }
     };
 
     const cancel = () => {
@@ -44,12 +89,33 @@ export default function AddFoodButtons() {
             <Button
                 mode="contained"
                 style={styles.button}
-                buttonColor={theme.colors.error}
-                textColor={theme.colors.onError}
+                buttonColor={theme.colors.secondary}
+                textColor={theme.colors.onSecondary}
                 onPress={cancel}
             >
                 Cancel
             </Button>
+            <Portal>
+                <Dialog
+                    visible={isDialogVisible}
+                    onDismiss={() => setDialogVisible(false)}
+                    style={{
+                        backgroundColor: theme.colors.surfaceVariant,
+                    }}
+                >
+                    <Dialog.Title>Missing fields</Dialog.Title>
+                    <Dialog.Content>
+                        <Paragraph style={{ color: theme.colors.onSurface }}>
+                            Please fill out all required fields.
+                        </Paragraph>
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                        <Button onPress={() => setDialogVisible(false)}>
+                            OK
+                        </Button>
+                    </Dialog.Actions>
+                </Dialog>
+            </Portal>
         </View>
     );
 }
