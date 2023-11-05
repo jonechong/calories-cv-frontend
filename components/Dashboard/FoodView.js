d2104d65c424c4e027fda053813fa7aa4ebef0cf
Jone <jone.chong99@gmail.com>
2023-11-05 22:16:54 +0800

    feat: delete function on long press

--- components/Dashboard/FoodView.js
+++ components/Dashboard/FoodView.js
@@ -6,11 +6,16 @@ import {
     Card,
     TouchableRipple,
     Title,
+    Portal,
+    Dialog,
+    Paragraph,
+    Button,
 } from "react-native-paper";
 import { useFocusEffect } from "@react-navigation/core";
-import { fetchDb } from "../../dbFunctions";
+import { fetchDb, deleteFoodLog } from "../../dbFunctions";
 import DbContext from "../../context/DbContext";
 import { useNavigation } from "@react-navigation/native";
+import * as FileSystem from "expo-file-system";
 
 export default function FoodView({ date }) {
     const navigation = useNavigation();
@@ -34,6 +39,83 @@ export default function FoodView({ date }) {
             borderRadius: 10,
         },
     });
+
+    //////////////////////////////////////////////////////////////////////////////
+    // For deletion confirmation dialog
+    const [dialogVisible, setDialogVisible] = useState(false);
+    const [itemToDelete, setItemToDelete] = useState(null);
+
+    const confirmDeletion = (item) => {
+        setItemToDelete(item);
+        setDialogVisible(true);
+    };
+
+    const deleteOldImage = async (imageUri) => {
+        try {
+            await FileSystem.deleteAsync(imageUri, { idempotent: true });
+            console.log(`Deleted old image`);
+        } catch (error) {
+            console.error(`Error deleting old image: ${error.message}`);
+        }
+    };
+
+    const deleteItem = () => {
+        deleteFoodLog(itemToDelete.log_id)
+            .then(() => {
+                const originalImageUri = itemToDelete.image_uri;
+                if (originalImageUri) {
+                    // Only delete the old image if there is an old image
+                    return deleteOldImage(originalImageUri);
+                }
+            })
+            .then(() => {
+                // After successful deletion, refetch the food data to update the list
+                return fetchDb(date);
+            })
+            .then((records) => {
+                // Update state with new data, this will cause a re-render of the component
+                setFoodData(records);
+            })
+            .catch((error) => {
+                console.error(
+                    "Error deleting the food log or deleting old image:",
+                    error
+                );
+            })
+            .finally(() => {
+                // Close the dialog after all operations
+                setDialogVisible(false);
+            });
+    };
+
+    // Render function for the confirmation dialog
+    const renderConfirmationDialog = () => (
+        <Portal>
+            <Dialog
+                visible={dialogVisible}
+                onDismiss={() => setDialogVisible(false)}
+                style={{
+                    backgroundColor: theme.colors.surfaceVariant,
+                }}
+            >
+                <Dialog.Title>Confirm Deletion</Dialog.Title>
+                <Dialog.Content>
+                    <Paragraph style={{ color: theme.colors.onSurface }}>
+                        Are you sure you want to delete this item?
+                    </Paragraph>
+                </Dialog.Content>
+                <Dialog.Actions>
+                    <Button onPress={() => setDialogVisible(false)}>
+                        Cancel
+                    </Button>
+                    <Button onPress={deleteItem}>Delete</Button>
+                </Dialog.Actions>
+            </Dialog>
+        </Portal>
+    );
+
+    //////////////////////////////////////////////////////////////////////////////
+
     const altImg = require("../../assets/no_img_avail.png");
     const [foodData, setFoodData] = useState([]);
     const [imageLoadError, setImageLoadError] = useState({});
@@ -113,6 +195,7 @@ export default function FoodView({ date }) {
                 <TouchableRipple
                     key={index.toString()}
                     onPress={() => editItem(item)}
+                    onLongPress={() => confirmDeletion(item)}
                     style={styles.touchable}
                     rippleColor={`${theme.colors.onSurface}1F`} // Ripple color with low opacity
                     borderless={true}
@@ -151,6 +234,8 @@ export default function FoodView({ date }) {
                     ? foodData.map(renderFoodItem)
                     : renderEmptyData()}
             </View>
+
+            {renderConfirmationDialog()}
         </Card>
     );
 }
