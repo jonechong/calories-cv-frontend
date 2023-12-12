77adabe9b4f9bb3b0283d110e5c0e0b0724a7a3d
jonechong <jone.chong99@gmail.com>
2023-12-13 00:33:27 +0800

    feat: implemented backend api calls for food detection

--- components/dashboard/buttons/DetectCaloriesButton.js
+++ components/dashboard/buttons/DetectCaloriesButton.js
@@ -9,6 +9,8 @@ import {
     useActionSheet,
 } from "@expo/react-native-action-sheet";
 
+const backendUrl = "http://192.168.1.38:8000";
+
 export default function DetectCaloriesButton({ styles, theme }) {
     const navigation = useNavigation();
     const { showActionSheetWithOptions } = useActionSheet();
@@ -69,32 +71,89 @@ export default function DetectCaloriesButton({ styles, theme }) {
     };
 
     const detectFoodProps = async (imageObject) => {
-        //This is where you call your backend service to detect your food properties
-        //Dummy data since backend is not implemented yet
-        const dummyFoodProps = {
-            foodName: "Dummy food name",
-            calories: 1,
-            protein: 2,
-            fats: 3,
-            carbs: 4,
-            imageUri: imageObject.uri,
-        };
-        return dummyFoodProps;
+        try {
+            // Extract the file extension from the image URI
+            const fileExtension = imageObject.uri
+                .split(".")
+                .pop()
+                .toLowerCase();
+
+            // Determine the MIME type based on the file extension
+            let mimeType = "image/jpeg"; // Default to JPEG
+            if (fileExtension === "png") {
+                mimeType = "image/png";
+            } else if (fileExtension === "jpg" || fileExtension === "jpeg") {
+                mimeType = "image/jpeg";
+            } else if (fileExtension === "bmp") {
+                mimeType = "image/bmp";
+            } else if (fileExtension === "gif") {
+                mimeType = "image/gif";
+            }
+
+            // Prepare the form data
+            let formData = new FormData();
+            formData.append("file", {
+                uri: imageObject.uri,
+                type: mimeType,
+                name: `image.${fileExtension}`,
+            });
+
+            // Make the fetch request to the FastAPI server
+            let response = await fetch(backendUrl + "/predict", {
+                method: "POST",
+                body: formData,
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+
+            let responseJson = await response.json();
+
+            if (!response.ok) {
+                throw new Error(responseJson.error || "API call failed");
+            }
+
+            return {
+                ...responseJson,
+                foodName: responseJson.prediction,
+                imageUri: imageObject.uri,
+            };
+        } catch (error) {
+            console.error("Error in detectFoodProps:", error);
+            throw error;
+        }
     };
 
-    const saveImage = (imageObject) => {
+    const saveImage = async (imageObject) => {
         console.log("Saved image URI:", imageObject.uri);
-        // Dummy backend data
-        detectFoodProps(imageObject)
-            .then((foodProps) => {
-                navigation.navigate("AddFood", {
-                    foodProps: foodProps,
-                    date: new Date().toISOString(),
-                });
-            })
-            .catch((error) => {
-                console.log(error);
+        try {
+            const foodProps = await detectFoodProps(imageObject);
+            // Replace underscores with spaces and capitalize each word
+            const formattedFoodName = foodProps.prediction
+                .replace(/_/g, " ")
+                .split(" ")
+                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(" ");
+
+            const modifiedFoodProps = {
+                ...foodProps,
+                calories: 1,
+                protein: 2,
+                fats: 3,
+                carbs: 4,
+                foodName: formattedFoodName,
+                imageUri: foodProps.imageUri,
+            };
+
+            console.log("Modified Food props:", modifiedFoodProps);
+
+            navigation.navigate("AddFood", {
+                foodProps: modifiedFoodProps,
+                date: new Date().toISOString(),
             });
+        } catch (error) {
+            console.error("Error in saveImage:", error);
+        }
     };
 
     return (
